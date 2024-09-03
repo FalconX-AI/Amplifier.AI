4759d0d743fe3ba4ccfca6ba4e6789efcb097180
Ahthesham Ali <ahtheshamali786@gmail.com>
2024-09-02 20:58:05 -0400

    added forms and sign-up components folder in compononets, also made a form-provider.tsx for SignUpFormProvider

---
+++ src/components/forms/sign-up/form-provider.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const SignUpFormProvider = (props: Props) => {
+  return (
+    <div>form-provider</div>
+  )
+}
+
+export default SignUpFormProvider
