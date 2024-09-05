ee5962aa9632b9db70d1acd817c5794b7094d92f
Ahthesham Ali <ahtheshamali786@gmail.com>
2024-09-05 10:09:46 -0400

    completed the landing page, some polishing needs to be done

---
+++ src/constants/landing-page.ts
@@ -0,0 +1,30 @@
+export const pricingCards = [
+    {
+      title: 'Standard',
+      description: 'Perfect for trying out Amplifier AI',
+      price: '$100',
+      duration: '',
+      highlight: 'Key features',
+      features: [' 1 domain ', '10 contacts', '10 Emails per month'],
+      priceId: '',
+    },
+    {
+      title: 'Ultimate',
+      description: 'The ultimate startup kit',
+      price: '$997',
+      duration: 'month',
+      highlight: 'Key features',
+      features: [' Unlimited domain ', '500 Contacts', '500 Emails'],
+  
+      priceId: 'price_1OYxkqFj9oKEERu1KfJGWxgN',
+    },
+    {
+      title: 'Plus',
+      description: 'For serious startup owners',
+      price: '$67',
+      duration: 'month',
+      highlight: 'Everything in Starter, plus',
+      features: [' 2 domain ', '50 contacts', '50 Emails per month'],
+      priceId: 'price_1OYxkqFj9oKEERu1NbKUxXxN',
+    },
+  ]
