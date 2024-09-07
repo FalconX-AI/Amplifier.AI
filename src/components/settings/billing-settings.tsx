aee7d16f9ab6d177e0e615e4ce60cc0d7d1dcd94
Ahthesham Ali <ahtheshamali786@gmail.com>
2024-09-07 16:27:30 -0400

    completed the bread-crumbs.tsx and settings page: WIP-> need to add stripe billing form

--- src/components/settings/billing-settings.tsx
+++ src/components/settings/billing-settings.tsx
@@ -11,8 +11,8 @@ import Image from 'next/image'
 type Props = {}
 
 const BillingSettings = async (props: Props) => {
+  //WIP: Need to add stripe subscription form
   const plan = await onGetSubscriptionPlan()
-  
   return (
     <div className="grid grid-cols-1 lg:grid-cols-5 gap-10">
       <div className="lg:col-span-1">
@@ -20,6 +20,35 @@ const BillingSettings = async (props: Props) => {
           label="Billing settings"
           message="Add payment information, upgrade and modify your plan."
         />
+      </div>
+            <div className="lg:col-span-2 flex justify-start lg:justify-center ">
+        {/* <Modal
+          title="Choose A Plan"
+          description="Tell us about yourself! What do you do? Let’s tailor your experience so it best suits you."
+          trigger={ 
+            plan && plan === 'STANDARD' ? (*/}
+              <Card className="border-dashed bg-cream border-gray-400 w-full cursor-pointer h-[270px] flex justify-center items-center">
+                <CardContent className="flex gap-2 items-center">
+                  <div className="rounded-full border-2 p-1">
+                    <Plus className="text-gray-400" />
+                  </div>
+                  <CardDescription className="font-semibold">
+                    Upgrade Plan
+                  </CardDescription>
+                </CardContent>
+              </Card>
+            {/* ) : ( 
+              <Image
+                src="/images/creditcard.png"
+                width={400}
+                height={400}
+                alt="image"
+              />
+            )
+          }
+        >
+          <SubscriptionForm plan={plan!} />
+        </Modal>*/}
       </div>
     </div>
   )
