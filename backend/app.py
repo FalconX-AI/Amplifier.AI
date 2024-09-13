23d3068fe0ebd2c6eb1a5ee3a0b4401c15c99e40
John Victor Stephen Durairaj <jh312868@dal.ca>
2024-09-13 02:21:07 -0400

    changed log path

--- backend/app.py
+++ backend/app.py
@@ -11,7 +11,7 @@ import logging
 load_dotenv()
 
 
-logging.basicConfig(filename='backend/app.log', level=logging.INFO)
+logging.basicConfig(filename='app.log', level=logging.INFO)
 
 app = Flask(__name__)
 CORS(app)
